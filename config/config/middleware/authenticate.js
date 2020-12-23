7ebae49e4dd227b1bbd17e4d43b02e3badcdf8d8
Aaron Lu <aaronnoh0409@gmail.com>
2020-12-22 22:23:21 -0500

    idk what am i doing at this point

--- config/config/middleware/authenticate.js
+++
@@ -1,8 +0,0 @@
-//middleware restricts routes a user is not allowed to visit if not logged in
-module.exports = function(req, res, next) {
-  if (req.user) {
-    return next();
-  }
-
-  return res.redirect("/login");
-};
